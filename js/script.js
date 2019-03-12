a59768e0aedfeb215f780f1120662419dedeb74e
Job Groen <jobgroen98@gmail.com>
2019-03-12 11:12:30 +0100

    Header

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ jQuery = $;
 
 $( document ).ready(function() {
     console.log( "ready!" );
-
     $("p").fadeIn( 2000 );
+});
+
+$(".navigatie-zoek").click(function(){
+    $(".navigatie-zoeken").show();
+    $(".navigatie-zoek").hide();
+    $(".navigatie-close").show();
+});
+
+$(".navigatie-close").click(function(){
+    $(".navigatie-close").hide();
+    $(".navigatie-zoek").show();
+    $(".navigatie-zoeken").hide();
 });
