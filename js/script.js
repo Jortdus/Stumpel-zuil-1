629e34c6c03542f2783ff1f566fa9c8f23cbd555
Job Groen <jobgroen98@gmail.com>
2019-03-12 11:48:46 +0100

    Header v.2.1

--- js/script.js
+++ js/script.js
@@ -5,16 +5,3 @@ $( document ).ready(function() {
     $("p").fadeIn( 2000 );
 });
 
-$(".navigatie-zoek").click(function(){
-    $(".navigatie-zoeken").animate({
-        width: 'toggle'
-    });
-    $(".navigatie-zoek").hide();
-    $(".navigatie-close").show();
-});
-
-$(".navigatie-close").click(function(){
-    $(".navigatie-close").hide();
-    $(".navigatie-zoek").show();
-    $(".navigatie-zoeken").hide();
-});
